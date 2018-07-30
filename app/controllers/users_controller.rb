9a6c3891ed1ce2358b5481a7b9a86812da5ccc0f
twinie96 <twinie96@gmail.com>
2018-07-30 17:12:44 +0200

    Fix redirection for logged in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,12 +39,14 @@ class UsersController < ApplicationController
     unless logged_in?
       store_location
       flash[:alert] = 'Please log in'
-      redirect_to login_url
+      redirect_to login_path
     end
   end
 
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(login_path) unless current_user?(@user)
+    store_location
+    # flash[:danger] = "Please log in."
+    redirect_to login_path unless current_user?(@user)
   end
 end
